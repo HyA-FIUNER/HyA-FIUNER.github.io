5cb1f706461a69979cbfba82adb271ab294fd2b0
Angel Zeitoune <angelzeitoune@gmail.com>
2024-03-31 15:27:07 -0300

    New component ModalGallery

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,31 +1,12 @@
-/*
-Markdown Gallery
--- v1.0 2016
--- Created by Lee Penney
--- Released under GPLv3
-*/
-
 import React, { useState } from 'react';
 import styles from './styles.module.css';
-
+import ModalGallery from '../ModalGallery';
 
 const Gallery = ({ images }) => {
 
     if (!images || images.length == 0) return null;
 
     const [slideIndex, setSlideIndex] = useState();
-
-    // Next/previous controls
-    const plusSlides = (n) => {
-        console.log('slideIndex', slideIndex, n);
-        if (slideIndex + n > images.length) {
-            setSlideIndex(1);
-        } else if (slideIndex + n < 1) {
-            setSlideIndex(images.length);
-        } else {
-            setSlideIndex(slideIndex + n);
-        }
-    };
 
     // set image with based on number of images
     const imageWith = 100 / images.length + '%';
@@ -44,24 +25,7 @@ const Gallery = ({ images }) => {
                 }
             </div>
 
-            {slideIndex &&
-                <div className={styles.modal}>
-                    <span className={styles.close} onClick={() => setSlideIndex(undefined)}>&times;</span>
-                    <div className={styles.modalContent}>
-                        <div className={styles.slide}>
-                            <div className={styles.numbertext} >{slideIndex} / {images.length}</div>
-                            <img className={styles.modalImage} src={images[slideIndex - 1].src} alt={images[slideIndex - 1].caption} />
-                        </div>
-
-                        <a className={styles.prev} onClick={() => plusSlides(-1)}>&#10094;</a>
-                        <a className={styles.next} onClick={() => plusSlides(1)}>&#10095;</a>
-
-                        <div className={styles.captionContainer}>
-                            <p id="caption">{images[slideIndex - 1].caption}</p>
-                        </div>
-                    </div>
-                </div>
-            }
+            {slideIndex && <ModalGallery images={images} slideIndex={slideIndex} setSlideIndex={setSlideIndex} />}
         </div>
     );
 };
---
+++ src/components/ModalGallery/index.js
@@ -0,0 +1,56 @@
+/*
+Markdown Gallery
+-- v1.0 2016
+-- Created by Lee Penney
+-- Released under GPLv3
+*/
+
+import React, { useState } from 'react';
+import styles from './styles.module.css';
+
+
+const ModalGallery = ({ images, slideIndex, setSlideIndex }) => {
+
+    if (!images || images.length == 0) return null;
+
+    // Next/previous controls
+    const plusSlides = (n) => {
+        if (slideIndex + n > images.length) {
+            setSlideIndex(1);
+        } else if (slideIndex + n < 1) {
+            setSlideIndex(images.length);
+        } else {
+            setSlideIndex(slideIndex + n);
+        }
+    };
+
+    // set image with based on number of images
+    const imageWith = 100 / images.length + '%';
+
+    return (
+        <div className={styles.modal}>
+            <span className={styles.close} onClick={() => setSlideIndex(undefined)}>&times;</span>
+            <div className={styles.modalContent}>
+                <div className={styles.slide}>
+                    <div className={styles.numbertext} >{images[slideIndex - 1].caption}</div>
+                    <img className={styles.modalImage} src={images[slideIndex - 1].src} alt={images[slideIndex - 1].caption} />
+                </div>
+
+                <a className={styles.prev} onClick={() => plusSlides(-1)}>&#10094;</a>
+                <a className={styles.next} onClick={() => plusSlides(1)}>&#10095;</a>
+
+                {
+                    images.map((image, index) => (
+                        <div key={index} className={styles.column} style={{ width: imageWith }}>
+                            <div className={styles.captionContainer} onClick={() => setSlideIndex(index + 1)}>
+                                <p id="caption">{image.caption}</p>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    );
+};
+
+export default ModalGallery;
