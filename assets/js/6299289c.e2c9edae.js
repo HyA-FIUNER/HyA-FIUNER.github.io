0a6d73af2c6fa71328340796e4b2a5253024ecdb
SchvemerEric <ericschvemer@outlook.com>
2024-04-14 20:19:40 -0300

    Deploy website - based on de3968d011a601f3b73fcf41b558aad7127597ca

---
+++ assets/js/6299289c.e2c9edae.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkhya_fiuner_github_io=self.webpackChunkhya_fiuner_github_io||[]).push([[483],{6292:a=>{a.exports=JSON.parse('{"title":"SOAM","slug":"/category/soam","permalink":"/docs/category/soam","navigation":{"next":{"title":"cabeza y cuello","permalink":"/docs/anatomia/SOAM/cabeza y cuello"}}}')}}]);
